c492ac29cb8e3c3d9c4527075ed5baf4644169fb
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-07-27 21:35:41 +0530

    first commit

---
+++ lua/ayush/lazy/luasnip.lua
@@ -0,0 +1,61 @@
+local ls = require "luasnip"
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+
+ls.add_snippets("cpp", {
+    s("include", t(
+        [[
+#include <bits/stdc++.h>
+using namespace std;
+using namespace chrono;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+typedef long long ll;
+
+#ifdef LOCAL
+#include "/home/ayush/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+struct custom_hash {static uint64_t splitmix64(uint64_t x) {x += 0x9e3779b97f4a7c15; x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9; x = (x ^ (x >> 27)) * 0x94d049bb133111eb; return x ^ (x >> 31);} size_t operator()(uint64_t x) const { static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count(); return splitmix64(x + FIXED_RANDOM);}};
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+void solve() {
+	
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("error.txt", "w", stderr);
+#endif
+    fastio();
+    auto start1 = high_resolution_clock::now();
+    int TC = 1;
+    cin >> TC;
+    while(TC--)
+        solve();
+    auto stop1 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop1 - start1);
+#ifdef LOCAL
+    cerr << "Time: " << duration.count() / 1000 << endl;
+#endif
+}
+]] ))
+})
+
+return{}
