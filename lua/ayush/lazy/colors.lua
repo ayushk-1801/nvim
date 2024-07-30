8df46ab1d6190a71591b240b2bae043f041004bf
ayushk-1801 <ayush23162@iiitd.ac.in>
2024-07-30 22:04:03 +0530

    Moved colorscheme to colors.lua

--- lua/ayush/lazy/colors.lua
+++ lua/ayush/lazy/colors.lua
@@ -1,9 +1,9 @@
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine-moon"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
@@ -85,5 +85,10 @@ return {
         end
     },
 
-
+    {
+        "bluz71/vim-moonfly-colors",
+        name = "moonfly",
+        lazy = false,
+        priority = 1000,
+    }
 }
