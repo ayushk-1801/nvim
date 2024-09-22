181613d861226a4d4d1253994fa340e419b4b8e0
ayush-1801 <ayush23162@iiitd.ac.in>
2024-09-22 23:04:04 +0530

    Fixed harpoon

--- lua/ayush/lazy/harpoon.lua
+++ lua/ayush/lazy/harpoon.lua
@@ -1,17 +1,26 @@
 return {
     "ThePrimeagen/harpoon",
+    lazy = false,
     config = function()
-        local harpoon = require("harpoon")
+        local mark = require "harpoon.mark"
+        local ui = require "harpoon.ui"
+        require("telescope").load_extension "harpoon"
 
-        harpoon.setup()
+        vim.keymap.set("n", "<leader>a", mark.add_file)
+        vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
+        vim.keymap.set("n", "<leader>ph", ":Telescope harpoon marks<CR>")
 
-        vim.keymap.set("n", "<leader>a", function() harpoon.add_file() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui.toggle_quick_menu() end)
-
-        for i = 1, 4 do
-            vim.keymap.set("n", "<C-" .. string.char(104 + i - 1) .. ">", function() harpoon.ui.nav_file(i) end)
-            vim.keymap.set("n", "<leader><C-" .. string.char(104 + i - 1) .. ">", function() harpoon.replace_file(i) end)
-        end
-    end
+        vim.keymap.set("n", "<leader>1", function()
+            ui.nav_file(1)
+        end)
+        vim.keymap.set("n", "<leader>2", function()
+            ui.nav_file(2)
+        end)
+        vim.keymap.set("n", "<leader>3", function()
+            ui.nav_file(3)
+        end)
+        vim.keymap.set("n", "<leader>4", function()
+            ui.nav_file(4)
+        end)
+    end,
 }
-
